72939c24df7554460d75a9daa6a2209ba754b623
Bill Merrill <bill.merrill@zapier.com>
2021-12-01 15:04:11 -0800

    make runnable

--- 1/onea.py
+++ 1/onea.py
@@ -1,14 +1,10 @@
->>> fh = open ('input.txt', 'r')
->>> depths = rh.readlines()
+fh = open ('input.txt', 'r')
+depths = fh.readlines()
 
+depth_count = 0; prev = 999999
+for depth in depths:
+    if int(depth) > prev:
+            depth_count += 1
+    prev = int(depth)
 
->>> depth_count = 0; prev = 999999
-
->>> fh.seek(0)
-0
->>> for depth in depths:
-...     if int(depth) > prev:
-...             depth_count += 1
-...     prev = int(depth)
-...
->>> depth_count
+print(depth_count)
