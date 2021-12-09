cc4917e003310a87554054c8bea3bec6d8fffa0a
Bill Merrill <bill.merrill@zapier.com>
2021-12-09 10:58:30 -0800

    add some experiments

--- 9/nineb.py
+++ 9/nineb.py
@@ -39,10 +39,7 @@ def compute_size(seafloor, origin):
         left = np.array([[0,1,0], [1,0,0], [0,1,0]])
         right = np.array([[0,1,0], [0,0,1], [0,1,0]])
         #top
-        top_neighbor = (pt[0]-1, pt[1])
-        if top_neighbor[0] < 0 ||
-            canvas[top_neighbor] == 9:
-            top = False
+        top_neighbor[0] < 0 ||
 
         #bottom
         bottom_neighbor = (pt[0]+1, pt[1])
@@ -62,3 +59,53 @@ for basin_origin in basin_origins:
 
 
 
+# >>> top
+# array([[0, 1, 0],
+#        [1, 0, 1],
+#        [0, 0, 0]])
+# >>> k = seafloor[1:4, 1:4]
+# >>> k
+# array([[9, 8, 7],
+#        [8, 5, 6],
+#        [7, 6, 7]])
+# >>> k * top
+# array([[0, 8, 0],
+#        [8, 0, 6],
+#        [0, 0, 0]])
+# >>> k[2,2]
+# 7
+# >>> k[1,1]
+# 5
+# >>> k[1,1] < (k*top)
+# array([[False,  True, False],
+#        [ True, False,  True],
+#        [False, False, False]])
+# >>> kans = _
+# >>> kans * top
+# array([[0, 1, 0],
+#        [1, 0, 1],
+#        [0, 0, 0]])
+# >>> sum(kans * top)
+# array([1, 1, 1])
+# >>> (kans * top).sum()
+# 3
+# >>> (kans * top).sum() == top.sum()
+# True
+# >>> k = seafloor[0:3, 0:3]
+# >>> kans = k[1,1] < (k*top)
+# >>> kans
+# array([[False, False, False],
+#        [False, False, False],
+#        [False, False, False]])
+# >>> k
+# array([[2, 1, 9],
+#        [3, 9, 8],
+#        [9, 8, 5]])
+# >>> (kans * top).sum() == top.sum()
+# False
+# >>> seafloor
+# array([[2, 1, 9, 9, 9, 4, 3, 2, 1, 0],
+#        [3, 9, 8, 7, 8, 9, 4, 9, 2, 1],
+#        [9, 8, 5, 6, 7, 8, 9, 8, 9, 2],
+#        [8, 7, 6, 7, 8, 9, 6, 7, 8, 9],
+#        [9, 8, 9, 9, 9, 6, 5, 6, 7, 8]])
